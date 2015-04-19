2ce57b0fd5c9cfc6787ec3ed74250e7849ca03a9
Steve SanPietro <sanpietro.steve@gmail.com>
2015-04-18 23:02:03 -0500

    More progress on the Post page

--- poolapp/apps/post/urls.py
+++ poolapp/apps/post/urls.py
@@ -3,20 +3,5 @@ from django.conf.urls import patterns, url
 from poolapp.apps.post import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.index, name='index'),
-  url(r'^names$', views.detail, name='detail'),
-
-  # Posting content
-  # Name
-  # Manually - not using the forms.py approach
-  url(r'^name/upload.html$', views.name_upload, name='name_upload'),
-  # Using the forms.py approach
-  url(r'^name/name_form_upload.html$',
-        views.name_form_upload, name='name_form_upload'),
-
-  # Tourney
-  # Using the forms.py approach
-  url(r'^tourney_form_upload.html$',
-        views.tourney_form_upload, name='tourney_form_upload'),
-
+  url(r'^$', views.tourney_form_upload, name='tourney_form_upload'),
 )
--- poolapp/apps/post/views.py
+++ poolapp/apps/post/views.py
@@ -3,54 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from poolapp.apps.post.models import Name, Tourney
-from poolapp.apps.post.forms import NameForm, TourneyForm
-
-def index(request):
-  return render(request, 'post/index.html', {})
-
-def detail(request):
-  name_list = Name.objects.all()
-  context = {'name_list': name_list}
-  return render(request, 'post/detail.html', context)
-
-# http://www.pythoncentral.io/writing-views-to-upload-posts-for-your-first-python-django-application/
-# from django.core.urlresolvers import reverse
-# from django.http import HttpResponseRedirect
-  
-# Add the following function to the end of myblog/views.py
-def name_upload(request):
-  if request.method == 'GET':
-    return render(request, 'post/name_upload.html', {})
-  elif request.method == 'POST':
-    name = Name.objects.create(your_name=request.POST['name'],
-        your_address=request.POST['address'])
-    # No need to call post.save() at this point -- it's already saved.
-#     return HttpResponseRedirect(reverse('post/detail'))
-    return render(request, 'post/success.html', {})
-
-### Using Forms
-
-def name_form_upload(request):
-  if request.method == 'GET':
-    form = NameForm()
-  else:
-    # A POST request: Handle Form Upload
-    form = NameForm(request.POST) # Bind data from request.POST into a PostForm
- 
-    # If data is valid, proceeds to create a new post and redirect the user
-    if form.is_valid():
-      your_name = form.cleaned_data['your_name']
-      your_address = form.cleaned_data['your_address']
-      name = Name.objects.create(your_name=your_name,
-                     your_address=your_address)
-      return render(request, 'post/success.html', {})
- 
-  return render(request, 'post/name_form_upload.html', {
-    'form': form,
-  })
-
-### Tourney Form Upload
+from poolapp.apps.post.models import Tourney
+from poolapp.apps.post.forms import TourneyForm
 
 def tourney_form_upload(request):
   if request.method == 'GET':
