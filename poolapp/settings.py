9e0b6072772d0aa1de3ca997d837c77ae143f780
Steve SanPietro <sanpietro.steve@gmail.com>
2015-05-17 14:11:22 -0500

    Fixed stupid https/http mixup with amazon static url

--- poolapp/settings.py
+++ poolapp/settings.py
@@ -130,5 +130,5 @@ if USE_AMW_FOR_STATICFILES:
   AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
   AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
   STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+  S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
   STATIC_URL = S3_URL
