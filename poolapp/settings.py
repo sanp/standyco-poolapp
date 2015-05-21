9c47703ba31e8f5e1e8979b69b25aaf684fab6c6
Steve SanPietro <sanpietro.steve@gmail.com>
2015-05-21 01:51:59 -0500

    Removed the email password from the settings file...that was stupid

--- poolapp/settings.py
+++ poolapp/settings.py
@@ -136,4 +136,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'standyco.inc@gmail.com'
-EMAIL_HOST_PASSWORD = 'e0*KgN&7oO$@'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
