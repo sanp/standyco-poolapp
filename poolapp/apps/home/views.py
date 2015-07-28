0291fa29e9dc4d4379836e420d0e8895c72eb408
Steve SanPietro <sanpietro.steve@gmail.com>
2015-07-27 20:05:55 -0500

    Replaced homepage with more minimal design

--- poolapp/apps/home/views.py
+++ poolapp/apps/home/views.py
@@ -5,6 +5,6 @@ from django.template import RequestContext
 def index(request):
   # Tell the base.html template to use the special override_block_content layout
   # rather than the default layout on the rest ofthe pages
-  override_block_content = True
+  override_block_content = False
   context = {'override_block_content': override_block_content}
   return render(request, 'home/index.html', context)
